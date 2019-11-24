3e3923fc02265286fead39999bc362896ba64bcb
Max <karlsson.maximiliam@gmail.com>
2019-11-24 16:06:07 +0100

    Removed a blankspace

--- SpaceInvader.cpp
+++ SpaceInvader.cpp
@@ -4,8 +4,7 @@
 int main(int argc, char *argv[])
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING)) {
-		std::cout << "SDL chould not initialise! SDL Error: " << SDL_GetError << std::endl;
-	
+		std::cout << "SDL chould not initialise! SDL Error: " << SDL_GetError << std::endl;		
 	}
 	return EXIT_SUCCESS;
 }
