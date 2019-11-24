d2d7566059ba21ad4caff240b8e7c0d0cf229326
Max <karlsson.maximiliam@gmail.com>
2019-11-24 16:01:34 +0100

    Initial commit

---
+++ SpaceInvader.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <SDL.h>
+
+int main(int argc, char *argv[])
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING)) {
+		std::cout << "SDL chould not initialise! SDL Error: " << SDL_GetError << std::endl;
+	
+	}
+	return EXIT_SUCCESS;
+}
